67c49f0a4eefaf7b856d970a37fab7a239527d16
Makara Wang <makara15@gmail.com>
2014-03-17 22:14:44 +0800

    Renamed gulpfile to Gulpfile.

---
+++ Gulpfile.js
@@ -0,0 +1,47 @@
+require('should');
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+
+// Files.
+var src = 'src/**/*.coffee';
+var tests = 'test/*.mocha.js';
+
+// Compile coffee scripts.
+gulp.task('coffee', function() {
+    return gulp.src(src)
+        .pipe(coffeelint({
+            max_line_length: {
+                value: 100
+            },
+            indentation: {
+                value: 4
+            }
+        }))
+        .pipe(coffeelint.reporter())
+        .pipe(coffee({
+            bare: true
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('./'));
+});
+
+// Run jshint.
+gulp.task('jshint', function() {
+    return gulp.src(tests)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Run tests.
+gulp.task('mocha', ['coffee', 'jshint'], function() {
+    return gulp.src(tests)
+        .pipe(mocha({
+            reporter: 'spec'
+        }));
+});
+
+gulp.task('default', ['coffee', 'jshint', 'mocha']);
