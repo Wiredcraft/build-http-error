58801d17831d3dcd1167b235d52ed24bb7597a0a
Makara Wang <makara15@gmail.com>
2014-12-02 13:27:31 +0800

    Added and use gulp-coverage.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,8 +6,9 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var coffeelint = require('gulp-coffeelint');
-var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var coverage = require('gulp-coverage');
 
 // Files.
 var src = 'src/**/*.coffee';
@@ -16,34 +17,39 @@ var tests = 'test/*.mocha.js';
 // Compile coffee scripts.
 gulp.task('coffee', function() {
     return gulp.src(src)
-        .pipe(coffeelint({
-            max_line_length: {
-                value: 100
-            },
-            indentation: {
-                value: 4
-            }
-        }))
-        .pipe(coffeelint.reporter())
         .pipe(coffee({
             bare: true
         }).on('error', gutil.log))
         .pipe(gulp.dest('./'));
 });
 
-// Run jshint.
+// Lint (or hint as an alias).
+gulp.task('lint', ['coffeelint', 'jshint']);
+gulp.task('hint', ['coffeelint', 'jshint']);
+gulp.task('coffeelint', function() {
+    return gulp.src(src)
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter());
+});
 gulp.task('jshint', function() {
-    return gulp.src(tests)
+    return gulp.src(['Gulpfile.js', tests])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // Run tests.
-gulp.task('mocha', ['coffee', 'jshint'], function() {
+gulp.task('mocha', ['coffee'], function() {
     return gulp.src(tests)
+        .pipe(coverage.instrument({
+            pattern: ['index.js'],
+            debugDirectory: 'test/debug'
+        }))
         .pipe(mocha({
             reporter: 'spec'
+        }))
+        .pipe(coverage.report({
+            outFile: 'test/coverage.html'
         }));
 });
 
-gulp.task('default', ['coffee', 'jshint', 'mocha']);
+gulp.task('default', ['coffeelint', 'jshint', 'coffee', 'mocha']);
